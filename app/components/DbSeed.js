7cfc4d7240352819046229d8c10e2ce9c019f333
kemron-itc <kglasgow@infotechcaribbean.com>
2017-12-11 16:21:14 -0400

    Added Seed Data

--- app/components/DbSeed.js
+++ app/components/DbSeed.js
@@ -1,13 +1,42 @@
 const ingredients = require("../ingredientData");
 const FoodItemComponent = require("./FoodItem")
+const UserComponent = require('./UserProfile')
 const _ = require("lodash")
 
 async function seed() {
-    // items have not been inserted yet
+    addFoodItems();
+    addUser();
+};
+
+
+async function addFoodItems() {
     let item = await FoodItemComponent.findFoodItemsByName([ingredients[0]])
-    if (_.isEmpty(item)) {
+    if (_.isEmpty(item)) {  // items have not been inserted yet
         await FoodItemComponent.addIngredients(ingredients);
     }
-};
+}
+
+
+async function addUser() {
+
+    let prevUser = await UserComponent.getUserWithCredential("jjjamerson@hotmail.com", "password");
+    if (prevUser) {
+        return;
+    }
+
+    await UserComponent.registerNewUser(
+        "Jay Jonah",
+        "Jamerson",
+        "jjjamerson@hotmail.com",
+        "password"
+    );
+
+    let [user, foodItems] = await Promise.all([
+        UserComponent.getUserWithCredential("jjjamerson@hotmail.com", "password"),
+        FoodItemComponent.findAllFoodItems().limit(3)
+    ]);
+    await UserComponent.addAllergens(user.id, foodItems.map(f => f.id))
+
+}
 
 module.exports = seed;
